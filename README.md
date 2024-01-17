0978d906b69245576553d94b99181500f1e5a5ae
Thuan Nguyen <thuan.nguyendinh89@gmail.com>
2024-01-17 21:57:13 +0700

    [BaiTap2] Tinh trung binh 5 so

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Khoi tao Git 'baiTapJsBuoi23'
 # Bai tap 1: Tinh tien luong NV
+# Bai tap 2: Tinh trung binh 5 so
--- js/main.js
+++ js/main.js
@@ -35,3 +35,38 @@ function tinhTienLuongNV() {
 }
 // Goi ham tinhTienLuongNV khi click button 'btnTinhTienLuongNV'
 document.getElementById('btnTinhTienLuongNV').onclick = tinhTienLuongNV;
+
+
+
+
+/**
+ * Bai 2 : Tính TB
+ * 
+ * Inputs:
+ * soThu1, soThu2, soThu3, soThu4, soThu5
+ * 
+ * Process:
+ * Function:
+ *   B1: Lấy giá trị từ form
+ *   B2: Lập công thức tính toán
+ *    + Xử lý ép kiểu number khi thực hiện phép (+)
+ *   B3: thông báo kết quả
+ * 
+ * Outputs:
+ * soTB
+ */
+ function tinhTB() {
+    let soThu1 = document.getElementById('soThu1').value;
+    let soThu2 = document.getElementById('soThu2').value;
+    let soThu3 = document.getElementById('soThu3').value;
+    let soThu4 = document.getElementById('soThu4').value;
+    let soThu5 = document.getElementById('soThu5').value;
+
+    let soTB = (Number(soThu1) + Number(soThu2) + Number(soThu3) + Number(soThu4) + Number(soThu5)) / 5;
+
+    // Su dung ${} cua ES6 de binding du lieu
+    let txtResult = document.getElementById('txtResultTinhTB');
+    txtResult.innerHTML = `<br /> Giá trị TB là : ${soTB.toLocaleString()}`;
+}
+// Goi ham tinhTB khi click button 'btnTinhTB'
+document.getElementById('btnTinhTB').onclick = tinhTB;
